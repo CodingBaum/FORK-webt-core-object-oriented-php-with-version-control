9b9d284e4a01e97eb3214443ce9d9bcbb6ce3ecc
Markus Wizany <9143@htl.rennweg.at>
2022-09-21 13:07:19 +0200

    nils ist noch mehr lost

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title> Video website dies das idk NILS IST LOST </title>
+        <link rel="stylesheet" href="style.css">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    </head>
+    <body>
+        <header> 
+            <h1>
+                NILS IST NICHT NUR LOST SONDERN AUCH BISSCHEN DEPPAT ♥
+            </h1>
+            <hr>
+        </header>
+        <div id="listBox">
+            <?php
+                
+            ?>
+        </div>
+    </body>
+    <script src="script.js"></script>
+</html>
