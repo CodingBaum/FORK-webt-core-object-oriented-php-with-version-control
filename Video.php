df95c070aff8c3ba9595abaae9500480dfc09444
Markus Wizany <9143@htl.rennweg.at>
2022-10-12 12:47:23 +0200

    COMPLETED || Core | Object Oriented PHP with Version Control

--- Video.php
+++ Video.php
@@ -1,14 +1,57 @@
 <?php
 
-class Video {
-    public  function __construct(
-        public string $testVariable = 'This is a test du scheißer'
-    ) {}
+interface IVideo {
+    public function getName();
+    public function getSource();
+    public function getHTMLCode();
+}
 
-    public function output() {
-        echo $this->testVariable;
+abstract class AVideo implements IVideo {
+    private $name;
+
+    public function getName() {
+        return $this->name;
+    }
+    abstract function getSource();
+    abstract function getHTMLCode();
+}
+
+class YoutubeVideo extends AVideo {
+    private $source;
+    private $name;
+
+    public  function __construct(string $name, string $source) {
+        $this->name = $name;
+        $this->source = $source;
+    }
+
+    function getSource()
+    {
+        return $this->source;
+    }
+
+    function getHTMLCode()
+    {
+        return '<iframe width="720" height="512" src="'.$this->source.'" title="'.$this->name.'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
     }
 }
 
-$video = new Video();
-$video->output();
+class Vimeo extends AVideo {
+    private $source;
+    private $name;
+
+    public  function __construct(string $name, string $source) {
+        $this->name = $name;
+        $this->source = $source;
+    }
+
+    function getSource()
+    {
+        return $this->source;
+    }
+
+    function getHTMLCode()
+    {
+        return '<iframe src="'.$this->source.'" width="720" height="512" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>';
+    }
+}
---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype HTML>
+<html lang="en">
+    <head>
+        <title> Video website dies das idk NILS IST LOST </title>
+        <link rel="stylesheet" href="style.css">
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    </head>
+    <body>
+        <header> 
+            <h1>
+                Das ist ein toller Titel
+            </h1>
+            <hr>
+        </header>
+        <div>
+            <?php
+                require ("Video.php");
+
+                $youtubeVideo = new YoutubeVideo("The Dragon Palace", 'https://www.youtube.com/embed/EoIhINb85kA');
+                echo $youtubeVideo->getHTMLCode();
+
+                $youtubeVideo = new YoutubeVideo("Mountain House", 'https://www.youtube.com/embed/JaTytztB0Nk');
+                echo $youtubeVideo->getHTMLCode();
+
+                $youtubeVideo = new YoutubeVideo("Wizard Tower", 'https://www.youtube.com/embed/EaRyDI0HjJY');
+                echo $youtubeVideo->getHTMLCode();
+
+                $youtubeVideo = new YoutubeVideo("Hobbit Hole", 'https://www.youtube.com/embed/rzeCdTZ5ylc');
+                echo $youtubeVideo->getHTMLCode();
+
+                $youtubeVideo = new YoutubeVideo("Beautiful Little Greenhouse Tutorial", 'https://www.youtube.com/embed/X6p65QXlI3A');
+                echo $youtubeVideo->getHTMLCode();
+
+                $vimeoVideo = new Vimeo("Oasis - a love letter to San Francisco", "https://player.vimeo.com/video/721214409?h=a4376a0ed2");
+                echo $vimeoVideo->getHTMLCode();
+
+                $vimeoVideo = new Vimeo("My Accomplice", "https://player.vimeo.com/video/731738424?h=f54a67bc9b&color=ffffff&title=0&byline=0&portrait=0&badge=0");
+                echo $vimeoVideo->getHTMLCode();
+
+                $vimeoVideo = new Vimeo("charlie hankin", "https://player.vimeo.com/video/758322686?h=88ffa414e2");
+                echo $vimeoVideo->getHTMLCode();
+
+                $vimeoVideo = new Vimeo("A Brief Disagreement", "https://player.vimeo.com/video/755602543?h=2422a5f12b");
+                echo $vimeoVideo->getHTMLCode();
+
+                $vimeoVideo = new Vimeo("Billie Eilish x Beat Saber", "https://player.vimeo.com/video/611934332?h=2851905f84&title=0&byline=0&portrait=0&badge=0");
+                echo $vimeoVideo->getHTMLCode();
+            ?>
+        </div>
+    </body>
+</html>
