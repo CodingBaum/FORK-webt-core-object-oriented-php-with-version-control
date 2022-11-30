c1328477870e7f3427a04d20839287a4f34db077
Nils Hubmann <nils.hubmann@htl.rennweg.at>
2022-11-30 11:56:03 +0100

    unique

--- CORE Server Responses with PHP in JSON format/Seeder.php
+++ CORE Server Responses with PHP in JSON format/Seeder.php
@@ -11,7 +11,7 @@ class Seeder {
         for ($i = 0; $i < 3; $i++) {
             $currentSongs = array();
             for ($j = 0; $j < 4; $j++) {
-                array_push($currentSongs, new Song($j, getRandomWord(), getRandomWord(), $j, mt_rand() / mt_getrandmax()));
+                array_push($currentSongs, new Song(uniqueID(), getRandomWord(), getRandomWord(), $j, mt_rand() / mt_getrandmax()));
             }
             array_push($this->OSTs, $currentSongs);
         }
@@ -20,6 +20,18 @@ class Seeder {
     public function getOSTs() {
         return $this->OSTs;
     }
+}
+function uniqueID(){
+    $zwerg=rand(1000,9999);
+    $arr=[];
+    if(in_array($zwerg,$arr,false)){
+        uniqueID();
+    }
+    else{
+        return $zwerg;
+    }
+
+
 }
 
 function getRandomWord($len = 10) {
