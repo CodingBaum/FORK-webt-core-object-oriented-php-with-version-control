cc5705f081f8fd6cc82ff8f1d40207b07c02c3e9
Markus Wizany <9143@htl.rennweg.at>
2023-03-08 12:00:27 +0100

    nils ist komisch

--- CORE Views in MVC/controller.php
+++ CORE Views in MVC/controller.php
@@ -9,16 +9,28 @@ array_push($hotels, new Hotel("Caesars Palace", "Dieses berühmte Casino-Hotel b
 array_push($hotels, new Hotel("MGM Grand Hotel", "Das gewaltige, smaragdfarbene Casino-Resort befindet sich am südlichen Ende des Vegas Strip gegenüber dem Tropicana. Der knapp 14 Meter hohe bronzene Löwe ist das Markenzeichen des Hotels und steht vor den Toren Wache."));
 array_push($hotels, new Hotel("The Mirage", "Dieses legendäre Casino-Hotel mit Blick auf den berühmten 'ausbrechenden Vulkan' befindet sich am Las Vegas Strip neben dem Hotel Caesars Palace und gegenüber von Madame Tussauds Las Vegas."));
 
-print(implode('
-', $hotels));
-
 $filename = "./view.html";
 $handle = fopen($filename, "r");
 $contents = fread($handle, filesize($filename));
 fclose($handle);
 
+$filename = "./templates/HotelView.html";
+$handle = fopen($filename, "r");
+$hotelTemplate = fread($handle, filesize($filename));
+fclose($handle);
+
+$loopContent = "";
+
+foreach ($hotels as $value) {
+    $content = str_replace("###HOTELNAME###", $value->getName(), $hotelTemplate);
+    $content = str_replace("###HOTELDESCRIPTION###", $value->getDescription(), $content);
+    $loopContent = $loopContent . $content;
+}
+
 $contents = str_replace("###HOTELNAME###", $hotels[0]->getName(), $contents);
 $contents = str_replace("###HOTELDESCRIPTION###", $hotels[0]->getDescription(), $contents);
+
+$contents = str_replace("###LOOP###", $loopContent, $contents);
 
 $filename = "./index.html";
 $handle = fopen($filename, "w");
