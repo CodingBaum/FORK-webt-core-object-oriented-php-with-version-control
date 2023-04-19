3b7db4f56ebf35223ffcd1544534fe9119fa4029
Markus Wizany <9143@htl.rennweg.at>
2023-04-19 12:58:02 +0200

    some progress but not really

---
+++ CORE doctrine dbal/addRecord.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html>
+<head>
+    <link rel="stylesheet" href="addStyle.css">
+</head>
+<body>
+<div id="container">
+    <label>
+        Name of Player 1:
+        <input name="player1" type="text">
+    </label>
+    <label>
+        Symbol chosen by Player 1:
+        <input name="symbol1" type="text">
+    </label>
+    <label>
+        Symbol chosen by Player 2:
+        <input name="symbol2" type="text">
+    </label>
+    <label>
+        Name of Player 2:
+        <input name="player2" type="text">
+    </label>
+    <input id="submit" type="button" value="Submit">
+</div>
+</body>
+</html>
--- CORE doctrine dbal/index.php
+++ CORE doctrine dbal/index.php
@@ -10,12 +10,13 @@
 <h3>Date: 01.01.2024</h3>
 <div id="rps-wrapper">
     <div id="rps-header" class="rps-game">
-        <div id="date">DATE</div>
-        <div id="name1">NAME1</div>
-        <div id="pick1">PICK1</div>
-        <div id="middle">:</div>
-        <div id="pick2">PICK2</div>
-        <div id="name2">NAME2</div>
+        <div class="id">ID</div>
+        <div class="date">DATE</div>
+        <div class="name1">NAME1</div>
+        <div class="pick1">PICK1</div>
+        <div class="middle">:</div>
+        <div class="pick2">PICK2</div>
+        <div class="name2">NAME2</div>
         <p></p>
     </div>
     <?php
@@ -31,14 +32,16 @@
     $loopContent = "";
 
     foreach ($allGames as $value) {
-        $content = str_replace("NAME1", getPlayerNameById($value['fk_pk_player1'])."", $rpsTemplate);
+        $content = str_replace("NAME1", getPlayerNameById($value['fk_pk_player1']) . "", $rpsTemplate);
+        $content = str_replace("ID", $value['pk_id'], $content);
         $content = str_replace("PICK1", $value['symbol1'], $content);
         $content = str_replace("PICK2", $value['symbol2'], $content);
-        $content = str_replace("NAME2", getPlayerNameById($value['fk_pk_player2'])."", $content);
-        $content = str_replace("DATE", $value['date']."", $content);
+        $content = str_replace("NAME2", getPlayerNameById($value['fk_pk_player2']) . "", $content);
+        $content = str_replace("DATE", $value['date'] . "", $content);
         echo $content;
     }
     ?>
 </div>
+<a href="addRecord.php">Add Record</a>
 </body>
 </html>
